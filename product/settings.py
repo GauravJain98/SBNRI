7abec16f430c370d4ac49a8cd7380286e524d9c1
Gaurav Jain <Gaurav.Jain1@evalueserve.com>
2020-11-19 16:40:45 +0530

    added THROTTLE

--- product/settings.py
+++ product/settings.py
@@ -104,7 +104,15 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
-}
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    },
+    }
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
